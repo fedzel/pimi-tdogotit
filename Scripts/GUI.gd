af8cd3598fb6bfe9bf98e15d300b5f6bb341cd71
Janne H <fedz@protonmail.com>
2019-12-13 23:44:32 +0200

    GUI
    GUI

---
+++ Scripts/GUI.gd
@@ -0,0 +1,11 @@
+extends Control
+
+#func _input(event):
+#	if event.is_action_pressed("left_click"):
+#		var poolint = get_node("ItemList").get_selected_items()
+#		for i in poolint:
+#			print(i)
+
+
+func _on_ItemList_item_selected(index):
+	print(index)
