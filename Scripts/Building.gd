30c4bceb2017a9bd872b5a1cf9211b8c487b8ff6
Joni Rajala <jonimatiasrajala@gmail.com>
2019-12-14 17:37:10 +0200

    Building do not smoke (all the time)

--- Scripts/Building.gd
+++ Scripts/Building.gd
@@ -1,12 +1,37 @@
 extends KinematicBody2D
 class_name Building
 
+var _maxHitpoints = 100
 var hitpoints = 100
 
+onready var _smokeParticle = get_node("SmokeParticle")
+
+enum damagestate {
+	nodamage,
+	damaged,
+	destroyed
+}
+
+var _prevState
+
+func _ready():
+	hitpoints = _maxHitpoints
+
 func _process(delta):
-	if isDestroyed():
-		visible = false
+	var newState = healthState() 
+	if newState != _prevState:
+		if newState == damagestate.destroyed:
+			visible = false
+		elif newState == damagestate.damaged:
+			_smokeParticle.emitting = true
+		else:
+			_smokeParticle.emitting = false
 		
-func isDestroyed():
-	return hitpoints <= 0
+func healthState():
+	if hitpoints <= 0:
+		return damagestate.destroyed
+	elif hitpoints < _maxHitpoints:
+		return damagestate.damaged
+	else:
+		return damagestate.nodamage
 
