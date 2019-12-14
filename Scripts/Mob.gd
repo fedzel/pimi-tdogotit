848a2a00fa6166aedb8e2a4fbf76830dfe223bdf
Joni Rajala <jonimatiasrajala@gmail.com>
2019-12-14 11:47:31 +0200

    Added bonfire and woods to World. Bullets are faster. Mobs inherit scaling.

--- Scripts/Mob.gd
+++ Scripts/Mob.gd
@@ -2,9 +2,9 @@ extends KinematicBody2D
 class_name Mob
 
 var target = Vector2(10,5)
+var _speed = 10
 
 func _physics_process(delta):
 	var pos = target - position
 	pos = pos.normalized()
-	var speed = 10
-	move_and_slide(pos * speed)
+	move_and_slide(pos * _speed)
