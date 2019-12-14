e03ac83e87c9a7f7be38c5bb1bd38451540b59f2
Joni Rajala <jonimatiasrajala@gmail.com>
2019-12-14 22:26:45 +0200

    Arrows despawn

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -3,9 +3,17 @@ extends KinematicBody2D
 var target = Vector2(10,5)
 var _speed = 10
 
+var _lifetime = 0
+const MAX_LIFETIME = 1.5
+
 func _ready():
 	set_meta("type", "bullet")
 	self._speed = 80
+	
+func _process(delta):
+	_lifetime += delta
+	if _lifetime > MAX_LIFETIME:
+		queue_free()
 
 func _physics_process(delta):
 	var pos = target - position
