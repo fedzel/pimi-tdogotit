41683168d012c5b167f985b90665cb80387a82b6
Janne H <fedz@protonmail.com>
2019-12-14 17:32:07 +0200

    Menu
    Menu

---
+++ Scripts/Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Scenes/World.tscn")
+
+
+func _on_Button2_pressed():
+	pass # Replace with function body.
+
+
+func _on_Button3_pressed():
+	get_tree().quit()
